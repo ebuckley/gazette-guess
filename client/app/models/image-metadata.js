f640f9dd54daed67a0e8c017d7fa9d9302f1f1db
Ersin Buckley <ersin@mediasuite.co.nz>
2016-07-31 13:05:01 +1200

    all the things

--- client/app/models/image-metadata.js
+++ client/app/models/image-metadata.js
@@ -1,7 +1,14 @@
-// /* global R */
 import DS from 'ember-data'
+import Ember from 'ember'
+import config from 'client/config/environment'
 const { attr } = DS
+const { computed, get } = Ember
 export default DS.Model.extend({
   updated_at: attr(),
-  created_at: attr()
+  created_at: attr(),
+  title: attr(),
+  displayDate: attr(),
+  imageUrl: computed('id', function () {
+    return config.apiHost + config.apiNamespace + '/image/' + get(this, 'id')
+  })
 })
--- client/app/src/index/route.js
+++ client/app/src/index/route.js
@@ -1,9 +1,11 @@
 import Ember from 'ember'
+import nlp from 'npm:nlp_compromise'
 const { get, set } = Ember
 
 export default Ember.Route.extend({
   page: 5,
   model: function () {
+    console.log('hello nlp', nlp)
     return this.store.query('ImageMetadata', {
       page: get(this, 'page')
     })
